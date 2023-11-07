75a143723af05d1819f57e29f72a5d92008131e0
noble-varghese <noblekvarghese96@gmail.com>
2023-11-07 17:30:37 +0530

    fix: adding the new openai params

--- src/providers/openai/chatComplete.ts
+++ src/providers/openai/chatComplete.ts
@@ -62,8 +62,19 @@ export const OpenAIChatCompleteConfig: ProviderConfig = {
   user: {
     param: "user",
   },
+  seed: {
+    param: "seed"
+  },
+  tools: {
+    param: "tools"
+  },
+  tool_choice: {
+    param: "tool_choice"
+  }
 };
 
-interface OpenAIChatCompleteResponse extends ChatCompletionResponse {}
+interface OpenAIChatCompleteResponse extends ChatCompletionResponse {
+  system_fingerprint: string;
+}
 
 export const OpenAIChatCompleteResponseTransform: (response: OpenAIChatCompleteResponse) => ChatCompletionResponse = (response) => response;
