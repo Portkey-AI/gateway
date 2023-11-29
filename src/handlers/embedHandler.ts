c3327707423e4258724d6a2b9b49349c4eb23b3e
visargD <visargdesai@gmail.com>
2023-11-29 14:51:16 +0530

    refactor embed handler

--- src/handlers/embedHandler.ts
+++ src/handlers/embedHandler.ts
@@ -1,24 +1,49 @@
 import { Context } from "hono";
-import { RequestBody } from "../types/requestBody";
 import { fetchProviderOptionsFromConfig, tryProvidersInSequence } from "./handlerUtils";
 
-export async function embedHandler(c: Context, env: any, request: RequestBody, requestHeaders: Record<string, string>): Promise<Response> {
+/**
+ * DEPRECATED
+ */
+export async function embedHandler(c: Context): Promise<Response> {
   try {
-
+    const request = await c.req.json();
+    const requestHeaders = Object.fromEntries(c.req.headers);
     let providerOptions = fetchProviderOptionsFromConfig(request.config)
     if (!providerOptions) {
-      const errorResponse = {
-        error: { message: `Could not find a provider option.`,}
-      };
-      throw errorResponse;
+      return new Response(JSON.stringify({
+        status: "failure",
+        message: "Could not find a provider option."
+      }), {
+        status: 400,
+        headers: {
+            "content-type": "application/json"
+        }
+      });
     }
-    const response = await tryProvidersInSequence(c, providerOptions, request, requestHeaders,  "embed");
 
-    return response;
-  } catch (error:any) {
-    // If an error occurs, log it and rethrow it
-    console.error(`Error in embedHandler: ${error.message}`);
-    const errorArray = JSON.parse(error.message);
-    throw errorArray[errorArray.length - 1];
+    try {
+      return await tryProvidersInSequence(c, providerOptions, request.params, requestHeaders,  "embed");
+    } catch (error:any) {
+      console.error(`embed error: ${error.message}`);
+      const errorArray = JSON.parse(error.message);
+      return new Response(errorArray[errorArray.length - 1].errorObj, {
+        status: errorArray[errorArray.length - 1].status,
+        headers: {
+            "content-type": "application/json"
+        }
+      });
+    }
+  } catch (err: any) {
+    return new Response(
+      JSON.stringify({
+          status: "failure",
+          message: "Something went wrong",
+      }), {
+          status: 500,
+          headers: {
+              "content-type": "application/json"
+          }
+      }
+    );
   }
 }
