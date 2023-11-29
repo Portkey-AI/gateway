076491ee16f95675b67ccb4149a7a10f17e03ad3
visargD <visargdesai@gmail.com>
2023-11-29 16:08:30 +0530

    add request validator for new route and configs

---
+++ src/middlewares/requestValidator/index.ts
@@ -0,0 +1,137 @@
+import { Context } from "hono";
+import {
+    ANTHROPIC,
+    ANYSCALE,
+    AZURE_OPEN_AI,
+    COHERE,
+    CONTENT_TYPES,
+    OPEN_AI,
+    PALM,
+    POWERED_BY,
+} from "../../globals";
+import { configSchema } from "./schema/config";
+
+export const requestValidator = (c: Context, next: any) => {
+    const requestHeaders = Object.fromEntries(c.req.headers);
+
+    if (
+        requestHeaders["content-type"] &&
+        ![
+            CONTENT_TYPES.APPLICATION_JSON,
+            CONTENT_TYPES.MULTIPART_FORM_DATA,
+        ].includes(requestHeaders["content-type"].split(";")[0])
+    ) {
+        return new Response(
+            JSON.stringify({
+                status: "failure",
+                message: `Invalid content type passed`,
+            }),
+            {
+                status: 400,
+                headers: {
+                    "content-type": "application/json",
+                },
+            }
+        );
+    }
+
+    if (
+        !(
+            requestHeaders[`x-${POWERED_BY}-config`] ||
+            requestHeaders[`x-${POWERED_BY}-provider`]
+        )
+    ) {
+        return new Response(
+            JSON.stringify({
+                status: "failure",
+                message: `Either (x-${POWERED_BY}-config) or x-${POWERED_BY}-provider) header is required`,
+            }),
+            {
+                status: 400,
+                headers: {
+                    "content-type": "application/json",
+                },
+            }
+        );
+    }
+    if (
+        requestHeaders[`x-${POWERED_BY}-provider`] &&
+        ![OPEN_AI, AZURE_OPEN_AI, COHERE, ANTHROPIC, ANYSCALE, PALM].includes(
+            requestHeaders[`x-${POWERED_BY}-provider`]
+        )
+    ) {
+        return new Response(
+            JSON.stringify({
+                status: "failure",
+                message: `Invalid provider passed`,
+            }),
+            {
+                status: 400,
+                headers: {
+                    "content-type": "application/json",
+                },
+            }
+        );
+    }
+
+
+    if (requestHeaders[`x-${POWERED_BY}-config`]) {
+        try {
+            const parsedConfig = JSON.parse(
+                requestHeaders[`x-${POWERED_BY}-config`]
+            );
+
+            const validatedConfig = configSchema.safeParse(parsedConfig);
+
+            if (
+                !validatedConfig.success &&
+                validatedConfig.error?.issues?.length
+            ) {
+                return new Response(
+                    JSON.stringify({
+                        status: "failure",
+                        message: `Invalid config passed`,
+                        errors: validatedConfig.error.issues.map(
+                            (e: any) => `path: ${e.path}, message: ${e.message}`
+                        ),
+                    }),
+                    {
+                        status: 400,
+                        headers: {
+                            "content-type": "application/json",
+                        },
+                    }
+                );
+            }
+
+            if (parsedConfig.options) {
+                return new Response(
+                    JSON.stringify({
+                        status: "failure",
+                        message: `This version of config is not supported in this route. Please migrate to the latest version`,
+                    }),
+                    {
+                        status: 400,
+                        headers: {
+                            "content-type": "application/json",
+                        },
+                    }
+                );
+            }
+        } catch (e) {
+            return new Response(
+                JSON.stringify({
+                    status: "failure",
+                    message: `Invalid config passed. You need to pass a valid json`,
+                }),
+                {
+                    status: 400,
+                    headers: {
+                        "content-type": "application/json",
+                    },
+                }
+            );
+        }
+    }
+    return next();
+};
---
+++ src/middlewares/requestValidator/schema/config.ts
@@ -0,0 +1,86 @@
+import { z } from "zod";
+import {
+    ANTHROPIC,
+    ANYSCALE,
+    AZURE_OPEN_AI,
+    COHERE,
+    OPEN_AI,
+} from "../../../globals";
+
+export const configSchema: any = z
+    .object({
+        strategy: z
+            .object({
+                mode: z
+                    .string()
+                    .refine(
+                        (value) =>
+                            ["single", "loadbalance", "fallback"].includes(
+                                value
+                            ),
+                        {
+                            message:
+                                "Invalid 'mode' value. Must be one of: single, loadbalance, fallback",
+                        }
+                    ),
+                on_status_codes: z.array(z.number()).optional(),
+            })
+            .optional(),
+        provider: z
+            .string()
+            .refine(
+                (value) =>
+                    [
+                        OPEN_AI,
+                        ANTHROPIC,
+                        AZURE_OPEN_AI,
+                        ANYSCALE,
+                        COHERE,
+                    ].includes(value),
+                {
+                    message:
+                        "Invalid 'provider' value. Must be one of: openai, anthropic, azure-openai, anyscale, cohere",
+                }
+            )
+            .optional(),
+        api_key: z.string().optional(),
+        cache: z
+            .object({
+                mode: z
+                    .string()
+                    .refine((value) => ["simple", "semantic"].includes(value), {
+                        message:
+                            "Invalid 'cache.mode' value. Must be one of: simple, semantic",
+                    }),
+                max_age: z.number().optional(),
+            })
+            .refine((value) => value.mode !== undefined, {
+                message: "'cache.mode' must be defined",
+            })
+            .optional(),
+        retry: z
+            .object({
+                attempts: z.number(),
+                on_status_codes: z.array(z.number()).optional(),
+            })
+            .refine((value) => value.attempts !== undefined, {
+                message: "'retry.attempts' must be defined",
+            })
+            .optional(),
+        weight: z.number().optional(),
+        on_status_codes: z.array(z.number()).optional(),
+        targets: z.array(z.lazy(() => configSchema)).optional(),
+    })
+    .refine(
+        (value) => {
+            const hasProviderApiKey =
+                value.provider !== undefined && value.api_key !== undefined;
+            const hasModeTargets =
+                value.strategy !== undefined && value.targets !== undefined;
+            return hasProviderApiKey || hasModeTargets;
+        },
+        {
+            message:
+                "Invalid configuration. It must have either 'provider' and 'api_key', or 'strategy' and 'targets'.",
+        }
+    );
