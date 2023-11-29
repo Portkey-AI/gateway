8428ce6d3f6e53965bbd915d072d60fb186a5f6e
visargD <visargdesai@gmail.com>
2023-11-29 14:50:52 +0530

    refactor complete handler

--- src/handlers/completeHandler.ts
+++ src/handlers/completeHandler.ts
@@ -1,34 +1,57 @@
-import { RequestBody } from "../types/requestBody";
 import { fetchProviderOptionsFromConfig, getProviderOptionsByMode, tryProvidersInSequence } from "./handlerUtils";
 import { Context } from "hono";
 
-// const OPENAI_CHAT_MODELS = ["gpt-4-0613","gpt-3.5-turbo-16k-0613","gpt-3.5-turbo-0301","gpt-3.5-turbo","gpt-3.5-turbo-0613","gpt-4","gpt-4-0314","gpt-3.5-turbo-16k","gpt-4-32k-0314","gpt-4-32k-0613","gpt-4-32k"]
-
 /**
+ * DEPRECATED
  * Handles the 'complete' API request by selecting the appropriate provider(s) and making the request to them.
  * If a provider is specified in the request config, that provider is used. Otherwise, the provider options are determined based on the mode in the request config.
  * If no provider options can be determined, an error is thrown. If the request to the provider(s) fails, an error is also thrown.
  *
- * @param {any} env - The cloudflare environment variables.
- * @param {RequestBody} request - The request body, which includes the config for the request (provider, mode, etc.).
  * @returns {Promise<CResponse>} - The response from the provider.
  * @throws Will throw an error if no provider options can be determined or if the request to the provider(s) fails.
  */
-export async function completeHandler(c: Context, env: any, request: RequestBody, requestHeaders: Record<string, string>): Promise<Response> {
-  const providerOptions = fetchProviderOptionsFromConfig(request.config)
-
-  if (!providerOptions) {
-    const errorResponse = {
-      error: { message: `Could not find a provider option.`,}
-    };
-    throw errorResponse;
-  }
-
+export async function completeHandler(c: Context): Promise<Response> {
   try {
-      return await tryProvidersInSequence(c, providerOptions, request, requestHeaders, "complete");
-  } catch (error:any) {
-    const errorArray = JSON.parse(error.message);
-    throw errorArray[errorArray.length - 1];
+    const request = await c.req.json();
+    const requestHeaders = Object.fromEntries(c.req.headers);
+    const providerOptions = fetchProviderOptionsFromConfig(request.config)
+
+    if (!providerOptions) {
+      return new Response(JSON.stringify({
+        status: "failure",
+        message: "Could not find a provider option."
+      }), {
+        status: 400,
+        headers: {
+            "content-type": "application/json"
+        }
+      });
+    }
+
+    try {
+      return await tryProvidersInSequence(c, providerOptions, request.params, requestHeaders, "complete");
+    } catch (error:any) {
+      const errorArray = JSON.parse(error.message);
+      return new Response(errorArray[errorArray.length - 1].errorObj, {
+        status: errorArray[errorArray.length - 1].status,
+        headers: {
+            "content-type": "application/json"
+        }
+      });
+    }
+  } catch (err: any) {
+    console.log("complete error", err.message);
+    return new Response(
+        JSON.stringify({
+            status: "failure",
+            message: "Something went wrong",
+        }), {
+            status: 500,
+            headers: {
+                "content-type": "application/json"
+            }
+        }
+    );
   }
 }
 
