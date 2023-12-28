39397ec2cfb85e1eac66a47df8fc8417de3e2d59
Rohit Agarwal <rohit@portkey.ai>
2023-12-28 17:55:28 +0530

    Allow port configuratoin while running the server

--- src/start-server.ts
+++ src/start-server.ts
@@ -4,9 +4,16 @@ import { serve } from '@hono/node-server'
 
 import app from './index'
 
+// Extract the port number from the command line arguments
+const defaultPort = 8787;
+const args = process.argv.slice(2);
+const portArg = args.find(arg => arg.startsWith('--port='));
+const port = portArg ? parseInt(portArg.split('=')[1]) : defaultPort;
+
+
 serve({
   fetch: app.fetch,
-  port: 8787,
+  port: port,
 })
 
-console.log("Your AI Gateway is now running on http://localhost:8787 🚀")
+console.log(`Your AI Gateway is now running on http://localhost:${port} 🚀`)
