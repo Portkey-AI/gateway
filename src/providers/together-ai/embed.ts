5314b6aeb536acfb3fabeae0a9f07545646e4910
visargD <visargdesai@gmail.com>
2024-01-31 17:08:16 +0530

    feat: support together-ai embeddings route

---
+++ src/providers/together-ai/embed.ts
@@ -0,0 +1,71 @@
+import { TOGETHER_AI } from "../../globals";
+import { EmbedParams, EmbedResponse } from "../../types/embedRequestBody";
+import { ErrorResponse, ProviderConfig } from "../types";
+import { TogetherAIErrorResponse } from "./chatComplete";
+
+export const TogetherAIEmbedConfig: ProviderConfig = {
+    model: {
+        param: "model",
+        required: true,
+        default: "mistral-embed",
+    },
+    input: {
+        param: "input",
+        required: true,
+        transform: (params: EmbedParams) => {
+            if (Array.isArray(params.input)) {
+                return params.input;
+            }
+
+            return [params.input];
+        },
+    },
+};
+
+interface TogetherAIEmbedResponse extends EmbedResponse {}
+
+export const TogetherAIEmbedResponseTransform: (
+    response: TogetherAIEmbedResponse | TogetherAIErrorResponse,
+    responseStatus: number
+) => EmbedResponse | ErrorResponse = (response, responseStatus) => {
+    if ("error" in response && responseStatus !== 200) {
+        return {
+            error: {
+                message: response.error,
+                type: null,
+                param: null,
+                code: null,
+            },
+            provider: TOGETHER_AI,
+        } as ErrorResponse;
+    }
+
+    if ("data" in response) {
+        return {
+            object: response.object,
+            data: response.data.map((d) => ({
+                object: d.object,
+                embedding: d.embedding,
+                index: d.index,
+            })),
+            model: response.model,
+            usage: {
+                prompt_tokens: 0,
+                total_tokens: 0,
+            },
+            provider: TOGETHER_AI,
+        };
+    }
+
+    return {
+        error: {
+            message: `Invalid response recieved from ${TOGETHER_AI}: ${JSON.stringify(
+                response
+            )}`,
+            type: null,
+            param: null,
+            code: null,
+        },
+        provider: TOGETHER_AI,
+    } as ErrorResponse;
+};
