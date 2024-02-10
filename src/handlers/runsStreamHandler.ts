267e24a3f2ff7dd41841eac18b1fdea799ebf7a6
Rohit Agarwal <rohit@portkey.ai>
2024-02-09 21:49:52 -0800

    Better code organization

--- src/handlers/runsStreamHandler.ts
+++ src/handlers/runsStreamHandler.ts
@@ -6,10 +6,33 @@ import { fetchProviderOptionsFromConfig, responseHandler, tryProvidersInSequence
 import { getStreamingMode } from "../utils";
 import { Config, ShortConfig } from "../types/requestBody";
 import { env } from "hono/adapter"
+
 // Find the proxy provider
-function proxyProvider(proxyModeHeader:string, providerHeader: string) {
-  const proxyProvider = proxyModeHeader?.split(" ")[1] ?? providerHeader;
+function proxyProvider(headers:{[k: string]: string;}) {
+  let proxyModeHeader = headers[HEADER_KEYS.MODE];
+  let providerHeader = headers[HEADER_KEYS.PROVIDER];
+  
+  let configHeader = headers[HEADER_KEYS.CONFIG];
+  let config;
+  try { config = JSON.parse(configHeader) } catch (err) { /* Ignore */ }
+  let configProvider = (config && 'provider' in config) ? config.provider : null;
+  
+  const proxyProvider = configProvider ?? proxyModeHeader?.split(" ")[1] ?? providerHeader;
   return proxyProvider;
+}
+
+function retryInfo(headers:{[k: string]: string;}) {
+  let configHeader = headers[HEADER_KEYS.CONFIG];
+  let config;
+  try { config = JSON.parse(configHeader) } catch (err) { /* Ignore */ }
+  let configRetry = (config && 'retry' in config && typeof config.retry === "object") ? config.retry : null;
+
+  let retryHeader = headers[HEADER_KEYS.RETRIES] ? parseInt(headers[HEADER_KEYS.RETRIES]) : null
+
+  let retryCount = Math.min((retryHeader ?? configRetry?.attempts ?? 1), MAX_RETRIES)
+  let retryStatusCodes = configRetry?.onStatusCodes ?? RETRY_STATUS_CODES
+
+  return {retryCount, retryStatusCodes}
 }
 
 function getProxyPath(requestURL:string, proxyProvider:string, proxyEndpointPath:string) {
@@ -69,6 +92,56 @@ function headersToSend(headersObj: Record<string, string>, customHeadersToIgnore
   return final;
 }
 
+// SSE Functions
+const sendEvent = (data: any, eventName:string|null = null, writer:WritableStreamDefaultWriter<any>, encoder: TextEncoder) => {
+  let message = '';
+  if (eventName) {
+    message += `event: ${eventName}\n`;
+  }
+  message += `data: ${JSON.stringify(data)}\n\n`;
+  writer.write(encoder.encode(message));
+};
+
+const initiateRun = async (basePath:string, fetchOptions:any) => {
+  try {
+    const response = await fetch(`${basePath}/runs`, fetchOptions);
+    const data: any = await response.json();
+    const run_id = data.id;
+    const thread_id = data.thread_id;
+    return {data,run_id,thread_id};
+  } catch (error) {
+    console.error('Error initiating run:', error);
+    return null;
+  }
+};
+
+const getRun = async (basePath:string, runId:string, threadId:string, headers:any) => {
+  let url = `${basePath}/${threadId}/runs/${runId}`;
+
+  const response = await fetch(url, {headers});
+  const data: any = await response.json();
+
+  return data;
+}
+
+const getSteps = async (basePath:string, runId:string, threadId:string, headers:any, lastStepRecd:string) => {
+  let url = `${basePath}/${threadId}/runs/${runId}/steps?before=${lastStepRecd}`;
+
+  const response = await fetch(url, {headers});
+  const data: any = await response.json();
+
+  return data.data;
+}
+
+const getMessages = async (basePath:string, runId:string, threadId:string, headers:any, lastMessageRecd:string) => {
+  let url = `${basePath}/${threadId}/messages?before=${lastMessageRecd}`;
+
+  const response = await fetch(url, {headers});
+  const data: any = await response.json();
+
+  return data.data;
+}
+
 export async function runsStreamHandler(c: Context): Promise < Response > {
   try {
     const requestHeaders = Object.fromEntries(c.req.raw.headers);
@@ -76,20 +149,12 @@ export async function runsStreamHandler(c: Context): Promise < Response > {
     const [requestJSON, requestFormData] = await getRequestData(c.req.raw, requestContentType);
 
     const store: Record < string, any > = {
-      proxyProvider: proxyProvider(requestHeaders[HEADER_KEYS.MODE], requestHeaders[`x-${POWERED_BY}-provider`]),
+      proxyProvider: proxyProvider(requestHeaders),
       reqBody: requestJSON,
       requestFormData: requestFormData,
       customHeadersToAvoid: env(c).CUSTOM_HEADERS_TO_IGNORE??[],
-      proxyPath: c.req.url.indexOf("/v1/proxy") > -1 ? "/v1/proxy" : "/v1"
+      proxyPath: "/v1"
     }
-
-    let requestConfig: Config | ShortConfig | null = null;
-    if (requestHeaders[`x-${POWERED_BY}-config`]) {
-      requestConfig = JSON.parse(requestHeaders[`x-${POWERED_BY}-config`]);
-      if (requestConfig && 'provider' in requestConfig) {
-        store.proxyProvider = requestConfig.provider;
-      }
-    };
 
     let urlToFetch = getProxyPath(c.req.url, store.proxyProvider, store.proxyPath);
     store.isStreamingMode = getStreamingMode(store.reqBody, store.proxyProvider, urlToFetch)
@@ -135,16 +200,8 @@ export async function runsStreamHandler(c: Context): Promise < Response > {
     //   }
     // }
 
-    let retryCount = 0;
-    let retryStatusCodes = RETRY_STATUS_CODES;
-    if (requestHeaders[HEADER_KEYS.RETRIES]) {
-      retryCount = parseInt(requestHeaders[HEADER_KEYS.RETRIES]);
-    } else if (requestConfig?.retry && typeof requestConfig.retry === "object") {
-      retryCount = requestConfig.retry?.attempts??1,
-        retryStatusCodes = requestConfig.retry?.onStatusCodes??RETRY_STATUS_CODES
-    }
-
-    retryCount = Math.min(retryCount, MAX_RETRIES);
+    // We're not retrying these for now
+    // let {retryCount, retryStatusCodes} = retryInfo(requestHeaders);
 
     // DO NOT CACHE THIS REQUEST
     // const getFromCacheFunction = c.get('getFromCache');
@@ -195,6 +252,7 @@ export async function runsStreamHandler(c: Context): Promise < Response > {
     let runMessages: any = [];
     let runSteps: any = [];
     let finalRunObj: any = {};
+    let intervalId:any;
 
     // Use TransformStream for handling SSE
     const {
@@ -204,102 +262,53 @@ export async function runsStreamHandler(c: Context): Promise < Response > {
     const writer = writable.getWriter();
     const encoder = new TextEncoder();
 
-    // Function to initiate run and send first event
-    const initiateRun = async () => {
-      try {
-        const response = await fetch(`${runBasePath}/runs`, fetchOptions);
-        const data: any = await response.json();
-        const run_id = data.id;
-        const thread_id = data.thread_id;
-        return {data,run_id,thread_id};
-      } catch (error) {
-        console.error('Error initiating run:', error);
-        return null;
-      }
-    };
-
-    // Function to send SSE data
-    const sendEvent = (data: any, eventName = null) => {
-      let message = '';
-      if (eventName) {
-        message += `event: ${eventName}\n`;
-      }
-      message += `data: ${JSON.stringify(data)}\n\n`;
-      writer.write(encoder.encode(message));
-    };
-
     // Function to poll APIs and send updates
     const pollAndUpdate = async (run_id: string, thread_id: string) => {
-      const urls = [
-        `${runBasePath}/${thread_id}/runs/${run_id}/steps?before=${lastStepRecd}`,
-        `${runBasePath}/${thread_id}/messages?before=${lastMessageRecd}`,
-        `${runBasePath}/${thread_id}/runs/${run_id}`,
-      ];
-
-      for (const url of urls) {
-        try {
-          const response = await fetch(url, {
-            headers: fetchOptions.headers
-          });
-          const data: any = await response.json();
-          sendEvent({"object": "ping"})
-
-          if (url === `${runBasePath}/${thread_id}/messages?before=${lastMessageRecd}`) {
-            let messages = data.data;
-            // console.log(JSON.stringify(messages.map(d => d.content[0]?.text?.value).filter(d => !!d)));
-            if (messages.length && !!messages[messages.length - 1].id) {
-              sendEvent(data);
-              let msgsToPush = messages.filter(m => !!m.content)
-              runMessages.push(...msgsToPush);
-              lastMessageRecd = messages[messages.length - 1].id
-            }
-          } else if (url === `${runBasePath}/${thread_id}/runs/${run_id}/steps?before=${lastStepRecd}`) {
-            let steps = data.data;
-            // console.log(JSON.stringify(steps.map(d => d.step_details).filter(d => !!d)));
-            let completedSteps = steps.filter(s => ['completed', 'cancelled', 'failed', 'expired'].includes(s.status))
-            if (completedSteps.length) {
-              sendEvent(data);
-              lastStepRecd = completedSteps[0].id
-              runSteps.push(...completedSteps)
-            }
-          } else {
-            sendEvent(data);
-          }
-
-          // Check if the run is completed, cancelled, failed, or expired
-          if (url === `${runBasePath}/${thread_id}/runs/${run_id}` && ['completed', 'cancelled', 'failed', 'expired'].includes(data.status)) {
-            finalRunObj = data;
-            finalRunObj.messages = runMessages;
-            finalRunObj.steps = runSteps;
-
-            console.log(JSON.stringify(finalRunObj))
-
-            writer.close(); // Close the stream
-            break; // Exit the loop
-          }
-        } catch (error) {
-          console.error(`Error fetching data from ${url}:`, error);
-          sendEvent({
-            error: `Error fetching data from ${url}`
-          });
+      try {
+        let steps = await getSteps(runBasePath, run_id, thread_id, fetchOptions.headers, lastStepRecd);
+        let completedSteps = steps.filter((s: { status: string; }) => ['completed', 'cancelled', 'failed', 'expired'].includes(s.status))
+        if (completedSteps.length) {
+          sendEvent(completedSteps, null, writer, encoder);
+          lastStepRecd = completedSteps[0].id
+          runSteps.push(...completedSteps)
         }
+
+        let messages = await getMessages(runBasePath, run_id, thread_id, fetchOptions.headers, lastMessageRecd);
+        let contentMessages = messages.filter((m: { content: any; }) => !!m.content);
+        if (contentMessages.length && !!contentMessages[0].id) {
+          sendEvent(contentMessages, null, writer, encoder);
+          runMessages.push(...contentMessages);
+          lastMessageRecd = contentMessages[0].id
+        }
+
+        let run = await getRun(runBasePath, run_id, thread_id, fetchOptions.headers);
+        if (['completed', 'cancelled', 'failed', 'expired'].includes(run.status)) {
+          finalRunObj = run;
+          finalRunObj.messages = runMessages;
+          finalRunObj.steps = runSteps;
+
+          console.log(JSON.stringify(finalRunObj))
+          clearInterval(intervalId)
+          writer.close(); // Close the stream
+        }
+      } catch (err) {
+        console.error(`Error fetching data:`, err);
+        sendEvent({error: `Error fetching data`}, 'error', writer, encoder);
       }
     };
 
     // Main logic to start the process and periodically poll for updates
     (async () => {
-      const initiationResult = await initiateRun();
+      const initiationResult = await initiateRun(runBasePath, fetchOptions);
       if (initiationResult) {
-        sendEvent(initiationResult.data); // Send the initiation data as the first SSE event
+        sendEvent(initiationResult.data, null, writer, encoder); // Send the initiation data as the first SSE event
         // Setup periodic polling for updates
         pollAndUpdate(initiationResult.run_id, initiationResult.thread_id)
-        const intervalId = setInterval(() => pollAndUpdate(initiationResult.run_id, initiationResult.thread_id), 5000);
+        intervalId = setInterval(() => pollAndUpdate(initiationResult.run_id, initiationResult.thread_id), 5000);
 
         // Additional logic to handle cleanup or stream closing can be added here if needed
       } else {
-        sendEvent({
-          error: 'Failed to initiate run'
-        }, 'error');
+        sendEvent({error: 'Failed to initiate run'}, 'error', writer, encoder);
         writer.close(); // Close the stream if initiation fails
       }
     })();
