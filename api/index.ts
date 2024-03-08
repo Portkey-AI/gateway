1a705198aaa9289256ce40457384e2eb4b9beace
James Pulec <jpulec@gmail.com>
2024-03-08 15:17:14 -0800

    fix: move ts

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,9 @@
 import { handle } from "hono/vercel";
 import app from "../src/index";
 
-export const runtime = "edge";
+export const config = {
+  runtime: "edge",
+};
 
 export const POST = handle(app);
 export default handle(app);
