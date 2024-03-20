c147f147d2e4c05583d1f6c5a8553f4237f54273
Lukas <git@lukasnotes.dk>
2024-03-20 08:11:16 +0100

    add prettier config

---
+++ .prettierrc.mjs
@@ -0,0 +1,15 @@
+/**
+ * @type {import('prettier').Options}
+ */
+export default {
+  semi: true,
+  tabWidth: 2,
+  printWidth: 80,
+  endOfLine: "lf",
+  singleQuote: true,
+  arrowParens: "always",
+  bracketSpacing: true,
+  trailingComma: "all",
+  proseWrap: "preserve",
+  htmlWhitespaceSensitivity: "ignore",
+}
