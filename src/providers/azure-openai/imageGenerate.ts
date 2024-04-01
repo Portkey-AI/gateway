604ef1d3f4a9ead2aa6008e3d53927b5e805d455
visargD <visargdesai@gmail.com>
2024-04-01 15:40:12 +0530

    feat: add azure-openai image generate model support

---
+++ src/providers/azure-openai/imageGenerate.ts
@@ -0,0 +1,56 @@
+import { AZURE_OPEN_AI } from '../../globals';
+import { OpenAIErrorResponseTransform } from '../openai/chatComplete';
+import { ErrorResponse, ImageGenerateResponse, ProviderConfig } from '../types';
+
+export const AzureOpenAIImageGenerateConfig: ProviderConfig = {
+  prompt: {
+    param: 'prompt',
+    required: true,
+  },
+  model: {
+    param: 'model',
+    required: true,
+    default: 'dall-e-3',
+  },
+  n: {
+    param: 'n',
+    min: 1,
+    max: 10,
+  },
+  quality: {
+    param: 'quality',
+  },
+  response_format: {
+    param: 'response_format',
+  },
+  size: {
+    param: 'size',
+  },
+  style: {
+    param: 'style',
+  },
+  user: {
+    param: 'user',
+  },
+};
+
+interface AzureOpenAIImageObject {
+  b64_json?: string; // The base64-encoded JSON of the generated image, if response_format is b64_json.
+  url?: string; // The URL of the generated image, if response_format is url (default).
+  revised_prompt?: string; // The prompt that was used to generate the image, if there was any revision to the prompt.
+}
+
+interface AzureOpenAIImageGenerateResponse extends ImageGenerateResponse {
+  data: AzureOpenAIImageObject[];
+}
+
+export const AzureOpenAIImageGenerateResponseTransform: (
+  response: AzureOpenAIImageGenerateResponse | ErrorResponse,
+  responseStatus: number
+) => ImageGenerateResponse | ErrorResponse = (response, responseStatus) => {
+  if (responseStatus !== 200 && 'error' in response) {
+    return OpenAIErrorResponseTransform(response, AZURE_OPEN_AI);
+  }
+
+  return response;
+};
