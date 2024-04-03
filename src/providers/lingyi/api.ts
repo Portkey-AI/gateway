1c85ceb24252129c818060ae5f00ababc1857963
Sterne Lee <xingguang@clare.ai>
2024-04-03 10:32:28 +0800

    feat: add api link

--- src/providers/lingyi/api.ts
+++ src/providers/lingyi/api.ts
@@ -3,7 +3,7 @@ import { ProviderAPIConfig } from '../types';
 const LingYiAPIConfig: ProviderAPIConfig = {
   getBaseURL: () => 'https://api.lingyiwanwu.com',
   headers: ({ providerOptions }) => {
-    return { Authorization: `Bearer ${providerOptions.apiKey}` };
+    return { Authorization: `Bearer ${providerOptions.apiKey}` }; // https://platform.lingyiwanwu.com/apikeys
   },
   getEndpoint: ({ fn }) => {
     switch (fn) {
--- src/providers/openrouter/api.ts
+++ src/providers/openrouter/api.ts
@@ -3,7 +3,7 @@ import { ProviderAPIConfig } from '../types';
 const OpenrouterAPIConfig: ProviderAPIConfig = {
   getBaseURL: () => 'https://openrouter.ai/api',
   headers: ({ providerOptions }) => {
-    return { Authorization: `Bearer ${providerOptions.apiKey}` };
+    return { Authorization: `Bearer ${providerOptions.apiKey}` }; // https://openrouter.ai/keys
   },
   getEndpoint: ({ fn }) => {
     switch (fn) {
