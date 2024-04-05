246df330cc036424e5a1fc550d762714347d41ce
visargD <visargdesai@gmail.com>
2024-04-05 12:35:30 +0530

    feat: add jina integration for embeddings and rerank

---
+++ src/providers/jina/embed.ts
@@ -0,0 +1,60 @@
+import { JINA } from '../../globals';
+import { EmbedResponse } from '../../types/embedRequestBody';
+import { ErrorResponse, ProviderConfig } from '../types';
+import {
+  generateErrorResponse,
+  generateInvalidProviderResponseError,
+} from '../utils';
+
+export const JinaEmbedConfig: ProviderConfig = {
+  model: {
+    param: 'model',
+    required: true,
+    default: 'jina-embeddings-v2-base-en',
+  },
+  input: {
+    param: 'input',
+    default: '',
+  },
+};
+
+interface JinaEmbedResponse extends EmbedResponse {}
+
+interface JinaErrorResponse {
+  detail: string;
+}
+
+export const JinaEmbedResponseTransform: (
+  response: JinaEmbedResponse | JinaErrorResponse,
+  responseStatus: number
+) => EmbedResponse | ErrorResponse = (response, responseStatus) => {
+  if (responseStatus !== 200 && 'detail' in response) {
+    return generateErrorResponse(
+      {
+        message: response.detail,
+        type: null,
+        param: null,
+        code: null,
+      },
+      JINA
+    );
+  }
+
+  if ('data' in response) {
+    return {
+      object: response.object,
+      data: response.data.map((d) => ({
+        object: d.object,
+        index: d.index,
+        embedding: d.embedding,
+      })),
+      model: response.model,
+      usage: {
+        prompt_tokens: response.usage?.prompt_tokens,
+        total_tokens: response.usage?.total_tokens,
+      },
+    };
+  }
+
+  return generateInvalidProviderResponseError(response, JINA);
+};
