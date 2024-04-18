ed0ba8a92f8c8e2e37e6ebb5502604b70abb4dfc
Meron Ogbai <22526062+meronogbai@users.noreply.github.com>
2024-04-18 13:55:27 +0300

    chore: pretty

--- src/providers/reka-ai/chatComplete.ts
+++ src/providers/reka-ai/chatComplete.ts
@@ -28,7 +28,15 @@ export const RekaAIChatCompleteConfig: ProviderConfig = {
       const messages: RekaMessageItem[] = [];
       let lastType: 'human' | 'model' | undefined;
 
-      const addMessage = ({ type, text, media_url }: { type: 'human' | 'model'; text: string; media_url?: string }) => {
+      const addMessage = ({
+        type,
+        text,
+        media_url,
+      }: {
+        type: 'human' | 'model';
+        text: string;
+        media_url?: string;
+      }) => {
         // NOTE: can't have more than one image in conversation history
         if (media_url && messages[0].media_url) {
           return;
@@ -39,9 +47,11 @@ export const RekaAIChatCompleteConfig: ProviderConfig = {
         if (lastType === type) {
           const placeholder: RekaMessageItem = {
             type: type === 'human' ? 'model' : 'human',
-            text: 'Placeholder for alternation'
+            text: 'Placeholder for alternation',
           };
-          media_url ? messages.unshift(placeholder) : messages.push(placeholder);
+          media_url
+            ? messages.unshift(placeholder)
+            : messages.push(placeholder);
         }
 
         // NOTE: image need to be first
@@ -49,17 +59,18 @@ export const RekaAIChatCompleteConfig: ProviderConfig = {
         lastType = type;
       };
 
-      params.messages?.forEach(message => {
-        const currentType: 'human' | 'model' = message.role === 'user' ? 'human' : 'model';
+      params.messages?.forEach((message) => {
+        const currentType: 'human' | 'model' =
+          message.role === 'user' ? 'human' : 'model';
 
         if (!Array.isArray(message.content)) {
           addMessage({ type: currentType, text: message.content || '' });
         } else {
-          message.content.forEach(item => {
+          message.content.forEach((item) => {
             addMessage({
               type: currentType,
               text: item.text || '',
-              media_url: item.image_url?.url
+              media_url: item.image_url?.url,
             });
           });
         }
@@ -68,7 +79,7 @@ export const RekaAIChatCompleteConfig: ProviderConfig = {
       if (messages[0].type !== 'human') {
         messages.unshift({
           type: 'human',
-          text: 'Placeholder for alternation'
+          text: 'Placeholder for alternation',
         });
       }
       return messages;
