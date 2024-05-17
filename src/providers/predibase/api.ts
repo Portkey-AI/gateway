1b06739eec6e62402686357d3be1526bb5793a55
Alex Sherstinsky <alexsherstinsky@users.noreply.github.com>
2024-05-17 08:11:57 -0700

    Fixing arguments.

--- src/providers/predibase/api.ts
+++ src/providers/predibase/api.ts
@@ -1,8 +1,8 @@
 import { ProviderAPIConfig } from '../types';
 
 const PredibaseAPIConfig: ProviderAPIConfig = {
-  // getBaseURL: () => 'https://serving.app.predibase.com/',
-  getBaseURL: () => 'https://serving.staging.predibase.com',
+  getBaseURL: () => 'https://serving.app.predibase.com',
+  // getBaseURL: () => 'https://serving.staging.predibase.com',
   headers: ({ providerOptions }) => {
     return {
       'Authorization': `Bearer ${providerOptions.apiKey}`,
@@ -12,8 +12,6 @@ const PredibaseAPIConfig: ProviderAPIConfig = {
   getEndpoint: ({ fn, gatewayRequestBody }) => {
     const user = gatewayRequestBody?.user;
     const model = gatewayRequestBody?.model;
-    console.log(fn)
-    console.log(gatewayRequestBody)
     switch (fn) {
       case 'chatComplete':
         return `/${user}/deployments/v2/llms/${model}/v1/chat/completions`;
