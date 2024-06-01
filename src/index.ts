96a5da4446d77a466ba69daea38f37500a193204
Rohit Agarwal <rohit@portkey.ai>
2024-06-01 18:39:36 +0530

    Added memory cache support in gateway based on conf.json

--- src/index.ts
+++ src/index.ts
@@ -22,6 +22,7 @@ import { hooks } from './middlewares/hooks';
 import { compress } from 'hono/compress';
 import { getRuntimeKey } from 'hono/adapter';
 import { imageGenerationsHandler } from './handlers/imageGenerationsHandler';
+import conf from '../conf.json'
 
 // Create a new Hono server instance
 const app = new Hono();
@@ -51,6 +52,10 @@ app.use('*', prettyJSON());
 
 // Use hooks middleware for all routes
 app.use('*', hooks);
+
+if (conf.cache) {
+  app.use('*', require('./middlewares/cache').memoryCache());
+}
 
 /**
  * Default route when no other route matches.
