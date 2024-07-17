0d9c4c2f12dadbad57abdd6fda94fa6ef6a0c3b3
Narendranath Gogineni <naren.g@portkey.ai>
2024-07-17 13:17:31 +0530

    formatting

--- src/providers/google-vertex-ai/chatComplete.ts
+++ src/providers/google-vertex-ai/chatComplete.ts
@@ -136,13 +136,12 @@ export const VertexGoogleChatCompleteConfig: ProviderConfig = {
           // @NOTE: This takes care of the "Please ensure that multiturn requests alternate between user and model."
           // error that occurs when we have multiple user messages in a row.
           const shouldCombineMessages =
-            lastRole === role &&
-            !params.model?.includes('vision');
+            lastRole === role && !params.model?.includes('vision');
 
           if (shouldCombineMessages) {
             messages[messages.length - 1].parts.push(...parts);
           } else {
-            messages.push({ role, parts });            
+            messages.push({ role, parts });
           }
 
           lastRole = role;
