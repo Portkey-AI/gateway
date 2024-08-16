e9df8d8d117c040b492a4afd1bfe3ca48fae4f07
Allan Murara <allan.murara@gmail.com>
2024-08-16 00:18:12 -0300

    fix pre-push test-server

--- start-test.js
+++ start-test.js
@@ -2,32 +2,21 @@ import { spawn } from 'node:child_process';
 
 console.log('Starting the application...');
 
-const app = spawn('node', ['build/start-server.js'], { stdio: ['pipe', 'pipe', process.stderr] });
+const app = spawn('node', ['build/start-server.js'], {
+  stdio: 'inherit',
+});
 
 // Listen for errors when spawning the process
-app.on('error', (err) => {
+app.on('exit', (err) => {
   console.error('Failed to start the app:', err);
   process.exit(1); // Exit with a failure code if there is an error
 });
 
-
 // Listen for when the process starts
-app.on('message', () => {
+app.on('spawn', () => {
+  console.log('App started successfully');
   setTimeout(() => {
-    console.log('App started successfully');
-    app.kill(); 
-    process.exit(0); 
-  }, 3000); 
-});
-
-// Clean up and ensure the process is killed if interrupted
-process.on('disconnect', () => {
-  console.log('END')
-  app.kill();
-});
-
-// Ensure the process is killed if the script is interrupted
-process.on('SIGINT', () => {
-  app.kill();
-  process.exit(1);
+    app.kill();
+    process.exit(0);
+  }, 3000);
 });
