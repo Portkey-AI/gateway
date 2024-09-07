7e804fad6f71bd8eed7f772fec80cc605410a0ba
Shaunak Das <51281688+shaun-ak@users.noreply.github.com>
2024-09-07 13:45:26 +0530

    fix formatting

--- plugins/default/alluppercase.ts
+++ plugins/default/alluppercase.ts
@@ -1,32 +1,31 @@
 import {
-    HookEventType,
-    PluginContext,
-    PluginHandler,
-    PluginParameters,
-  } from '../types';
-  import { getText } from '../utils';
-  
-  function isAllUpperCase(str: string): boolean {
-    // Remove non-letter characters and compare the result to its uppercased version
-    return str === str.toUpperCase();
+  HookEventType,
+  PluginContext,
+  PluginHandler,
+  PluginParameters,
+} from '../types';
+import { getText } from '../utils';
+
+function isAllUpperCase(str: string): boolean {
+  // Remove non-letter characters and compare the result to its uppercased version
+  return str === str.toUpperCase();
+}
+
+export const handler: PluginHandler = async (
+  context: PluginContext,
+  parameters: PluginParameters,
+  eventType: HookEventType
+) => {
+  let error = null;
+  let verdict = false;
+  let data = null;
+
+  try {
+    let text = getText(context, eventType);
+    verdict = isAllUpperCase(text);
+  } catch (e) {
+    error = e as Error;
   }
-  
-  export const handler: PluginHandler = async (
-    context: PluginContext,
-    parameters: PluginParameters,
-    eventType: HookEventType
-  ) => {
-    let error = null;
-    let verdict = false;
-    let data = null;
-  
-    try {
-      let text = getText(context, eventType);
-      verdict = isAllUpperCase(text);
-    } catch (e) {
-      error = e as Error;
-    }
-  
-    return { error, verdict, data };
-  };
-  
+
+  return { error, verdict, data };
+};
