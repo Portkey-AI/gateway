a680267585743eb91efece38507bae9fd68e766c
shivam-pareek <shivam.pareek2002@gmail.com>
2024-09-12 14:02:19 +0530

    feat/add-upstage-provider

---
+++ src/providers/upstage/api.ts
@@ -0,0 +1,17 @@
+import { ProviderAPIConfig } from '../types';
+
+export const upstageAPIConfig: ProviderAPIConfig = {
+  getBaseURL: () => 'https://api.upstage.ai/v1/solar',
+  headers({ providerOptions }) {
+    const { apiKey } = providerOptions;
+    return { Authorization: `Bearer ${apiKey}` };
+  },
+  getEndpoint({ fn }) {
+    switch (fn) {
+      case 'chatComplete':
+        return `/chat/completions`;
+      default:
+        return '';
+    }
+  },
+};
