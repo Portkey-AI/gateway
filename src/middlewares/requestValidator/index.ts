4a7135785234d21d0f7eeee47387fb2f68c3f753
Sterne Lee <sternelee@gmail.com>
2024-10-02 07:48:16 +0800

    feat: pass provider by model name

--- src/middlewares/requestValidator/index.ts
+++ src/middlewares/requestValidator/index.ts
@@ -1,9 +1,28 @@
 import { Context } from 'hono';
 import { CONTENT_TYPES, POWERED_BY, VALID_PROVIDERS } from '../../globals';
 import { configSchema } from './schema/config';
+import Providers from '../../providers';
 
-export const requestValidator = (c: Context, next: any) => {
+const providers = Object.keys(Providers);
+
+export const requestValidator = async (c: Context, next: any) => {
   const requestHeaders = Object.fromEntries(c.req.raw.headers);
+  const clonedReq = c.req.raw.clone();
+  const originalBody = await clonedReq.text();
+  const modifiedBody = JSON.parse(originalBody);
+  const [provider, ...modelNames] = modifiedBody.model.split(':');
+  if (providers.includes(provider)) {
+    requestHeaders[`x-${POWERED_BY}-provider`] = provider;
+    modifiedBody.model = modelNames.join(':') || undefined;
+    const newHeaders = new Headers(requestHeaders);
+    const newRequest = new Request(c.req.raw.url, {
+      method: c.req.raw.method,
+      headers: newHeaders,
+      body: JSON.stringify(modifiedBody),
+    });
+
+    c.req.raw = newRequest;
+  }
 
   const contentType = requestHeaders['content-type'];
   if (
@@ -151,5 +170,5 @@ export const requestValidator = (c: Context, next: any) => {
       );
     }
   }
-  return next();
+  return await next();
 };
