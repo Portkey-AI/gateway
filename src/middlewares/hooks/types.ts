39d33337199acd6e763d90284796371cc022629e
visargD <visargdesai@gmail.com>
2024-10-11 18:08:18 +0530

    chore: add plugin options interface

--- src/middlewares/hooks/types.ts
+++ src/middlewares/hooks/types.ts
@@ -1,6 +1,7 @@
 export interface Check {
   id: string;
   parameters: object;
+  is_enabled?: boolean;
 }
 
 export interface HookOnFailObject {
@@ -86,3 +87,7 @@ export interface GuardrailResult {
 export type HookResult = GuardrailResult;
 
 export type EventType = 'beforeRequestHook' | 'afterRequestHook';
+
+export interface HandlerOptions {
+  env: Record<string, any>;
+}
