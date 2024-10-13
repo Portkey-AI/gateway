777cd8c439446998ae4aa4c766be4209deedd16d
thev19 <vasundharasinghvasu@gmail.com>
2024-10-13 22:44:39 +0530

    added before request test

--- plugins/default/default.test.ts
+++ plugins/default/default.test.ts
@@ -779,4 +779,26 @@ describe('allLowercase handler', () => {
     expect(result.error).toBe(null);
     expect(result.verdict).toBe(false);
   });
+  it('should return true verdict for a sentence with all lowercase characters', async () => {
+    const context: PluginContext = {
+      request: { text: 'this is a sentence. this is another sentence' },
+    };
+    const eventType = 'beforeRequestHook';
+
+    const result = await allLowerCaseHandler(context, {}, eventType);
+
+    expect(result.error).toBe(null);
+    expect(result.verdict).toBe(true);
+  });
+  it('should return false verdict for a sentence with not all lowercase characters', async () => {
+    const context: PluginContext = {
+      request: { text: 'THIS IS A SENTENCE. THIS IS ANOTHER SENTENCE.' },
+    };
+    const eventType = 'beforeRequestHook';
+
+    const result = await allLowerCaseHandler(context, {}, eventType);
+
+    expect(result.error).toBe(null);
+    expect(result.verdict).toBe(false);
+  });
 });
