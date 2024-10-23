240e16c6d0a4bc27fafaf98d5c7d05cc40b947db
Narendranath Gogineni <narenrockstar1@gmail.com>
2024-10-23 15:52:33 +0530

    change response type of createdAt

--- src/providers/google-vertex-ai/imageGenerate.ts
+++ src/providers/google-vertex-ai/imageGenerate.ts
@@ -153,7 +153,7 @@ export const GoogleImageGenResponseTransform: (
 
   if ('predictions' in response) {
     return {
-      created: `${new Date().getTime()}`,
+      created: Math.floor(Date.now() / 1000),
       data: response.predictions.map((generation) => ({
         b64_json: generation.bytesBase64Encoded,
       })),
