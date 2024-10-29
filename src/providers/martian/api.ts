a7c5eace3ee49c934ea850bebfefa429ce1db0af
Luis C. Rojano <lcrojano@gmail.com>
2024-10-29 08:20:48 -0500

    feat(provider): add martian api

---
+++ src/providers/martian/api.ts
@@ -0,0 +1,24 @@
+import { ProviderAPIConfig } from '../types';
+
+const MartianAPIConfig: ProviderAPIConfig = {
+  getBaseURL: ({ providerOptions }) => {
+    return (
+      providerOptions.martianBaseUrl ||
+      'https://withmartian.com/api/openai/v1'
+    );
+  },
+  headers: ({ providerOptions }) => ({
+    Content-Type: 'application/json',
+    Authorization: `Bearer ${providerOptions.apiKey}`,
+  }),
+  getEndpoint: ({ fn }) => {
+    switch (fn) {
+      case 'chatComplete':
+        return '/chat/completions';
+      default:
+        return '';
+    }
+  },
+};
+
+export default MartianAPIConfig;
