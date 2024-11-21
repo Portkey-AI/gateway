8d77f6f8190d6901276d31ef7b42c584634111b0
Rohit Agarwal <rohit@portkey.ai>
2024-11-21 20:16:49 +0530

    Add Qdrant as a provider to the gateway

---
+++ src/providers/qdrant/index.ts
@@ -0,0 +1,11 @@
+import { ProviderConfigs } from '../types';
+import QdrantAPIConfig from './api';
+
+const QdrantConfig: ProviderConfigs = {
+  api: QdrantAPIConfig,
+  responseTransforms: {
+    
+  },
+};
+
+export default QdrantConfig;
