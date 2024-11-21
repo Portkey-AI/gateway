386810bdea834bd5501fa119a0e6ffb7146d1fbd
visargD <visargdesai@gmail.com>
2024-11-21 16:26:14 +0530

    feat: integrate realtime event parser class in realtime route handlers

--- src/handlers/realtimeHandler.ts
+++ src/handlers/realtimeHandler.ts
@@ -1,5 +1,4 @@
 import { Context } from 'hono';
-import { WSContext, WSEvents } from 'hono/ws';
 import { constructConfigFromRequestHeaders } from './handlerUtils';
 import { ProviderAPIConfig } from '../providers/types';
 import Providers from '../providers';
@@ -9,6 +8,7 @@ import {
   getOptionsForOutgoingConnection,
   getURLForOutgoingConnection,
 } from './websocketUtils';
+import { RealTimeLLMEventParser } from '../services/realtimeLLMEventParser';
 
 const getOutgoingWebSocket = async (url: string, options: RequestInit) => {
   let outgoingWebSocket: WebSocket | null = null;
@@ -28,7 +28,7 @@ const getOutgoingWebSocket = async (url: string, options: RequestInit) => {
 };
 
 export async function realTimeHandler(c: Context): Promise<Response> {
-  let requestHeaders = Object.fromEntries(c.req.raw.headers);
+  const requestHeaders = Object.fromEntries(c.req.raw.headers);
 
   const providerOptions = constructConfigFromRequestHeaders(
     requestHeaders
@@ -47,6 +47,17 @@ export async function realTimeHandler(c: Context): Promise<Response> {
     c
   );
 
+  const sessionOptions = {
+    id: crypto.randomUUID(),
+    providerOptions: {
+      ...providerOptions,
+      requestURL: url,
+      rubeusURL: 'realtime',
+    },
+    requestHeaders,
+    requestParams: {},
+  };
+
   const webSocketPair = new WebSocketPair();
   const client = webSocketPair[0];
   const server = webSocketPair[1];
@@ -54,8 +65,8 @@ export async function realTimeHandler(c: Context): Promise<Response> {
   server.accept();
 
   let outgoingWebSocket: WebSocket = await getOutgoingWebSocket(url, options);
-
-  addListeners(outgoingWebSocket, server, c, url);
+  const eventParser = new RealTimeLLMEventParser();
+  addListeners(outgoingWebSocket, eventParser, server, c, sessionOptions);
 
   return new Response(null, {
     status: 101,
--- src/handlers/realtimeHandlerNode.ts
+++ src/handlers/realtimeHandlerNode.ts
@@ -1,81 +1,97 @@
 import { Context } from 'hono';
-import { WSContext, WSEvents } from 'hono/ws';
 import { constructConfigFromRequestHeaders } from './handlerUtils';
 import WebSocket from 'ws';
 import { ProviderAPIConfig } from '../providers/types';
 import Providers from '../providers';
 import { Options } from '../types/requestBody';
-import { createRequestOption } from './websocketUtils';
+import { RealTimeLLMEventParser } from '../services/realtimeLLMEventParser';
+import { WSContext, WSEvents } from 'hono/ws';
 
 export async function realTimeHandlerNode(
   c: Context
 ): Promise<WSEvents<unknown>> {
-  let incomingWebsocket: WSContext<unknown> | null = null;
-  let events: { [key: string]: any }[] = [];
-  let requestHeaders = Object.fromEntries(c.req.raw.headers);
-  const camelCaseConfig = constructConfigFromRequestHeaders(requestHeaders);
+  try {
+    let incomingWebsocket: WSContext<unknown> | null = null;
+    const requestHeaders = Object.fromEntries(c.req.raw.headers);
+    const camelCaseConfig = constructConfigFromRequestHeaders(requestHeaders);
 
-  const provider = camelCaseConfig?.provider ?? '';
-  const apiConfig: ProviderAPIConfig = Providers[provider].api;
-  const providerOptions = camelCaseConfig as Options;
-  const baseUrl = apiConfig.getBaseURL({ providerOptions });
-  const endpoint = apiConfig.getEndpoint({
-    providerOptions,
-    fn: 'realtime',
-    gatewayRequestBody: {},
-    gatewayRequestURL: c.req.url,
-  });
-  let url = `${baseUrl}${endpoint}`;
-  url = url.replace('https://', 'wss://');
-  const headers = await apiConfig.headers({
-    c,
-    providerOptions,
-    fn: 'realtime',
-    transformedRequestUrl: url,
-    transformedRequestBody: {},
-  });
+    const provider = camelCaseConfig?.provider ?? '';
+    const apiConfig: ProviderAPIConfig = Providers[provider].api;
+    const providerOptions = camelCaseConfig as Options;
+    const baseUrl = apiConfig.getBaseURL({ providerOptions });
+    const endpoint = apiConfig.getEndpoint({
+      providerOptions,
+      fn: 'realtime',
+      gatewayRequestBody: {},
+      gatewayRequestURL: c.req.url,
+    });
+    let url = `${baseUrl}${endpoint}`;
+    url = url.replace('https://', 'wss://');
+    const headers = await apiConfig.headers({
+      c,
+      providerOptions,
+      fn: 'realtime',
+      transformedRequestUrl: url,
+      transformedRequestBody: {},
+    });
 
-  const outgoingWebSocket = new WebSocket(url, {
-    headers,
-  });
+    const sessionOptions = {
+      id: crypto.randomUUID(),
+      providerOptions: {
+        ...providerOptions,
+        requestURL: url,
+        rubeusURL: 'realtime',
+      },
+      requestHeaders,
+      requestParams: {},
+    };
 
-  outgoingWebSocket.addEventListener('message', (event) => {
-    incomingWebsocket?.send(event.data as string);
-    const parsedData = JSON.parse(event.data as string);
-    parsedData.createdAt = new Date();
-    events.push(parsedData);
-  });
+    const outgoingWebSocket = new WebSocket(url, {
+      headers,
+    });
+    const eventParser = new RealTimeLLMEventParser();
 
-  outgoingWebSocket.addEventListener('close', (event) => {
-    incomingWebsocket?.close();
-  });
-
-  outgoingWebSocket.addEventListener('error', (event) => {
-    console.log('outgoingWebSocket error', event);
-    incomingWebsocket?.close();
-  });
-
-  return {
-    onOpen(evt, ws) {
-      incomingWebsocket = ws;
-    },
-    onMessage(event, ws) {
-      outgoingWebSocket?.send(event.data as string);
+    outgoingWebSocket.addEventListener('message', (event) => {
+      incomingWebsocket?.send(event.data as string);
       try {
         const parsedData = JSON.parse(event.data as string);
-        parsedData.createdAt = new Date();
-        events.push(parsedData);
-      } catch (error) {
-        console.log('error parsing event', error);
+        eventParser.handleEvent(c, parsedData, sessionOptions);
+      } catch (err: any) {
+        console.error(`eventParser.handleEvent error: ${err.message}`);
       }
-    },
-    onError(evt, ws) {
-      console.log('realtimeHandler error', evt);
-      outgoingWebSocket?.close();
-    },
-    onClose(evt, ws) {
-      outgoingWebSocket?.close();
-      c.set('requestOptions', createRequestOption(url, events));
-    },
-  };
+    });
+
+    outgoingWebSocket.addEventListener('close', () => {
+      incomingWebsocket?.close();
+    });
+
+    outgoingWebSocket.addEventListener('error', (event) => {
+      console.error(`outgoingWebSocket error: ${event.message}`);
+      incomingWebsocket?.close();
+    });
+
+    return {
+      onOpen(evt, ws) {
+        incomingWebsocket = ws;
+      },
+      onMessage(event) {
+        outgoingWebSocket?.send(event.data as string);
+      },
+      onError(evt) {
+        console.error(`incomingWebsocket error: ${evt.type}`);
+        outgoingWebSocket?.close();
+      },
+      onClose() {
+        outgoingWebSocket?.close();
+      },
+    };
+  } catch (err) {
+    c.set('websocketError', true);
+    return {
+      onOpen() {},
+      onMessage() {},
+      onError() {},
+      onClose() {},
+    };
+  }
 }
