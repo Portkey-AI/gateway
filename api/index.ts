a6f07a511a5642d4f5d622956a87ea9898f99d47
James Pulec <jpulec@gmail.com>
2025-03-03 17:12:17 -0800

    chore: drop vercel stuff

--- api/index.ts
+++
@@ -1,9 +0,0 @@
-import { handle } from 'hono/vercel';
-import app from '../src/index';
-
-export const config = {
-  runtime: 'edge',
-};
-
-export const POST = handle(app);
-export const GET = handle(app);
