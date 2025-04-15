a3966b4f1c9f87935f592adba2f9e92e5b1773a7
James Pulec <jpulec@gmail.com>
2025-04-15 10:41:24 -0700

    fix: up keepalive

--- src/start-server.ts
+++ src/start-server.ts
@@ -150,10 +150,12 @@ const server = serve({
   overrideGlobalObjects: false,
   serverOptions: {
     // Ensure all inactive connections are terminated by the ALB, by setting this
-    // a few seconds higher than the ALB idle timeout
+    // a few seconds higher than the ALB idle timeout.
+    //
+    // Set to 5 minutes (300000ms) + 5 seconds (5000ms) to ensure that the ALB
     //
     // @see https://adamcrowder.net/posts/node-express-api-and-aws-alb-502/
-    keepAliveTimeout: 65000,
+    keepAliveTimeout: 1000 * 60 * 5 + 5000,
   },
 });
 
