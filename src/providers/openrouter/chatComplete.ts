9e273c288ffda4a9ef1288b56a628dec0d492920
unsync <1211591+unsync@users.noreply.github.com>
2025-04-27 12:22:28 +0200

    feature: return tool calls in non streaming response

--- src/providers/openrouter/chatComplete.ts
+++ src/providers/openrouter/chatComplete.ts
@@ -146,6 +146,12 @@ export const OpenrouterChatCompleteResponseTransform: (
         message: {
           role: c.message.role,
           content: c.message.content,
+          ...(c.message.reasoning && {
+            reasoning: c.message.reasoning,
+          }),
+          ...(c.message.tool_calls && {
+            tool_calls: c.message.tool_calls,
+          }),
         },
         finish_reason: c.finish_reason,
       })),
