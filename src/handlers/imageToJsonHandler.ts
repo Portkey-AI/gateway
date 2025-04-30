636bed7b5fb13f169af99ebeec48225d9125c063
unsync <1211591+unsync@users.noreply.github.com>
2025-04-30 13:38:56 +0200

    feat: if the imageToJson fails, return the original response

--- src/handlers/imageToJsonHandler.ts
+++ src/handlers/imageToJsonHandler.ts
@@ -5,22 +5,31 @@ export async function getTransformedResponse(_: {
   transformer: (base64Image: string) => Record<string, any>;
 }): Promise<Response | undefined> {
   const { response, transformer } = _;
-
-  console.info('imageToJsonHandler > converting image response to base64 JSON');
-  const imageBuffer = await response.arrayBuffer();
-  // Simple ArrayBuffer to base64 conversion for environments like Cloudflare Workers
-  let binary = '';
-  const bytes = new Uint8Array(imageBuffer);
-  const len = bytes.byteLength;
-  for (let i = 0; i < len; i++) {
-    binary += String.fromCharCode(bytes[i]);
+  try {
+    console.info(
+      'imageToJsonHandler > converting image response to base64 JSON'
+    );
+    const imageBuffer = await response.arrayBuffer();
+    // Simple ArrayBuffer to base64 conversion for environments like Cloudflare Workers
+    let binary = '';
+    const bytes = new Uint8Array(imageBuffer);
+    const len = bytes.byteLength;
+    for (let i = 0; i < len; i++) {
+      binary += String.fromCharCode(bytes[i]);
+    }
+    return new Response(JSON.stringify(transformer(btoa(binary))), {
+      headers: {
+        ...Object.fromEntries(response.headers), // keep original headers
+        'content-type': CONTENT_TYPES.APPLICATION_JSON,
+      },
+      status: response.status,
+      statusText: response.statusText,
+    });
+  } catch (error) {
+    console.error(
+      'imageToJsonHandler > error converting image response to base64 JSON',
+      error
+    );
+    return response;
   }
-  return new Response(JSON.stringify(transformer(btoa(binary))), {
-    headers: {
-      ...Object.fromEntries(response.headers), // keep original headers
-      'content-type': CONTENT_TYPES.APPLICATION_JSON,
-    },
-    status: response.status,
-    statusText: response.statusText,
-  });
 }
