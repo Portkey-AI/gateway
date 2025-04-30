0f2467925457de9672762bd71a324c22f9d04dfb
Nayonika Roy <nayonikaroy@Nayonikas-MacBook-Pro.local>
2025-04-30 16:19:06 -0700

    Add bedrock back

--- src/providers/index.ts
+++ src/providers/index.ts
@@ -75,6 +75,7 @@ const Providers: { [key: string]: ProviderConfigs } = {
   nomic: NomicConfig,
   ollama: OllamaAPIConfig,
   ai21: AI21Config,
+  bedrock: BedrockConfig,
   groq: GroqConfig,
   segmind: SegmindConfig,
   jina: JinaConfig,
