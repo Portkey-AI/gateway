88d6b5c544522a620aac9518fa11e7e0bbd6d334
Narendranath Gogineni <narenrockstar1@gmail.com>
2025-06-10 17:36:13 +0530

    multimodal embeds for cohere

--- src/providers/bedrock/embed.ts
+++ src/providers/bedrock/embed.ts
@@ -6,23 +6,56 @@ import { generateInvalidProviderResponseError } from '../utils';
 import { BedrockErrorResponseTransform } from './chatComplete';
 
 export const BedrockCohereEmbedConfig: ProviderConfig = {
-  input: {
-    param: 'texts',
-    required: true,
-    transform: (params: any): string[] => {
-      if (Array.isArray(params.input)) {
-        return params.input;
-      } else {
-        return [params.input];
-      }
+  input: [
+    {
+      param: 'texts',
+      required: false,
+      transform: (params: EmbedParams): string[] | undefined => {
+        if (typeof params.input === 'string') return [params.input];
+        else if (Array.isArray(params.input) && params.input.length > 0) {
+          const texts: string[] = [];
+          params.input.forEach((item) => {
+            if (typeof item === 'string') {
+              texts.push(item);
+            } else if (item.text) {
+              texts.push(item.text);
+            }
+          });
+          return texts.length > 0 ? texts : undefined;
+        }
+      },
     },
-  },
+    {
+      param: 'images',
+      required: false,
+      transform: (params: EmbedParams): string[] | undefined => {
+        if (Array.isArray(params.input) && params.input.length > 0) {
+          const images: string[] = [];
+          params.input.forEach((item) => {
+            if (typeof item === 'object' && item.image?.base64) {
+              images.push(item.image.base64);
+            }
+          });
+          return images.length > 0 ? images : undefined;
+        }
+      },
+    },
+  ],
   input_type: {
     param: 'input_type',
     required: true,
   },
   truncate: {
     param: 'truncate',
+    required: false,
+  },
+  encoding_format: {
+    param: 'embedding_types',
+    required: false,
+    transform: (params: any): string[] => {
+      if (Array.isArray(params.encoding_format)) return params.encoding_format;
+      return [params.encoding_format];
+    },
   },
 };
 
