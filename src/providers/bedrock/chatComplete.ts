1604efda5c1e1227c210d040d33eab61fcb26dcc
Narendranath Gogineni <narenrockstar1@gmail.com>
2025-06-17 11:00:35 +0530

    return finish reason in bedrock error chunk

--- src/providers/bedrock/chatComplete.ts
+++ src/providers/bedrock/chatComplete.ts
@@ -28,6 +28,7 @@ import {
 } from './complete';
 import { BedrockErrorResponse } from './embed';
 import {
+  getBedrockErrorChunk,
   transformAdditionalModelRequestFields,
   transformAI21AdditionalModelRequestFields,
   transformAnthropicAdditionalModelRequestFields,
@@ -584,6 +585,8 @@ export const BedrockChatCompleteResponseTransform: (
 };
 
 export interface BedrockChatCompleteStreamChunk {
+  // this is error message from bedrock
+  message?: string;
   contentBlockIndex?: number;
   delta?: {
     text: string;
@@ -640,6 +643,10 @@ export const BedrockChatCompleteStreamChunkTransform: (
   gatewayRequest
 ) => {
   const parsedChunk: BedrockChatCompleteStreamChunk = JSON.parse(responseChunk);
+  console.log(JSON.stringify(parsedChunk, null, 2));
+  if (parsedChunk.message) {
+    return getBedrockErrorChunk(fallbackId, gatewayRequest.model || '');
+  }
   if (parsedChunk.stopReason) {
     streamState.stopReason = parsedChunk.stopReason;
   }
