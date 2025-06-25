2f3768cb388b371437d8cce560030016aebc6252
Mahesh <maheshmahi19@outlook.com>
2025-06-25 13:28:28 +0530

    fix: add try/catch for batch output

--- src/providers/azure-ai-inference/getBatchOutput.ts
+++ src/providers/azure-ai-inference/getBatchOutput.ts
@@ -42,56 +42,83 @@ export const AzureAIInferenceGetBatchOutputRequestHandler = async ({
     transformedRequestUrl: retrieveBatchURL,
     gatewayRequestBody: {},
   });
-  const retrieveBatchesResponse = await fetch(retrieveBatchURL, {
-    method: 'GET',
-    headers: retrieveBatchesHeaders,
-  });
+  try {
+    const retrieveBatchesResponse = await fetch(retrieveBatchURL, {
+      method: 'GET',
+      headers: retrieveBatchesHeaders,
+    });
 
-  const batchDetails: RetrieveBatchResponse =
-    await retrieveBatchesResponse.json();
-
-  const outputFileId =
-    batchDetails.output_file_id || batchDetails.error_file_id;
-  if (!outputFileId) {
-    const errors = batchDetails.errors;
-    if (errors) {
-      return new Response(JSON.stringify(errors), {
-        status: 200,
-      });
+    if (!retrieveBatchesResponse.ok) {
+      const error = await retrieveBatchesResponse.text();
+      return new Response(
+        JSON.stringify({
+          error: error || 'error fetching batch output',
+          provider: AZURE_OPEN_AI,
+          param: null,
+        }),
+        {
+          status: 500,
+        }
+      );
     }
+
+    const batchDetails: RetrieveBatchResponse =
+      await retrieveBatchesResponse.json();
+
+    const outputFileId =
+      batchDetails.output_file_id || batchDetails.error_file_id;
+    if (!outputFileId) {
+      const errors = batchDetails.errors;
+      if (errors) {
+        return new Response(JSON.stringify(errors), {
+          status: 200,
+        });
+      }
+      return new Response(
+        JSON.stringify({
+          error: 'invalid response output format',
+          provider_response: batchDetails,
+          provider: AZURE_OPEN_AI,
+        }),
+        {
+          status: 400,
+        }
+      );
+    }
+    const retrieveFileContentRequestURL = `https://api.portkey.ai/v1/files/${outputFileId}/content`; // construct the entire url instead of the path of sanity sake
+    const retrieveFileContentURL =
+      baseUrl +
+      AzureAIInferenceAPI.getEndpoint({
+        providerOptions,
+        fn: 'retrieveFileContent',
+        gatewayRequestURL: retrieveFileContentRequestURL,
+        c,
+        gatewayRequestBodyJSON: {},
+        gatewayRequestBody: {},
+      });
+    const retrieveFileContentHeaders = await AzureAIInferenceAPI.headers({
+      c,
+      providerOptions,
+      fn: 'retrieveFileContent',
+      transformedRequestBody: {},
+      transformedRequestUrl: retrieveFileContentURL,
+      gatewayRequestBody: {},
+    });
+    const response = fetch(retrieveFileContentURL, {
+      method: 'GET',
+      headers: retrieveFileContentHeaders,
+    });
+    return response;
+  } catch (e) {
     return new Response(
       JSON.stringify({
-        error: 'invalid response output format',
-        provider_response: batchDetails,
+        error: 'error fetching batch output',
         provider: AZURE_OPEN_AI,
+        param: null,
       }),
       {
-        status: 400,
+        status: 500,
       }
     );
   }
-  const retrieveFileContentRequestURL = `https://api.portkey.ai/v1/files/${outputFileId}/content`; // construct the entire url instead of the path of sanity sake
-  const retrieveFileContentURL =
-    baseUrl +
-    AzureAIInferenceAPI.getEndpoint({
-      providerOptions,
-      fn: 'retrieveFileContent',
-      gatewayRequestURL: retrieveFileContentRequestURL,
-      c,
-      gatewayRequestBodyJSON: {},
-      gatewayRequestBody: {},
-    });
-  const retrieveFileContentHeaders = await AzureAIInferenceAPI.headers({
-    c,
-    providerOptions,
-    fn: 'retrieveFileContent',
-    transformedRequestBody: {},
-    transformedRequestUrl: retrieveFileContentURL,
-    gatewayRequestBody: {},
-  });
-  const response = fetch(retrieveFileContentURL, {
-    method: 'GET',
-    headers: retrieveFileContentHeaders,
-  });
-  return response;
 };
