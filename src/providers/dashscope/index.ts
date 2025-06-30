b6cc7cf8bd0d23068c124636274d76b90b0c5e32
horochx <32632779+horochx@users.noreply.github.com>
2025-06-30 15:13:03 +0800

    feat: The Qwen model supports parameters such as `enable_thinking` and `enable_search`, and can invoke a rerank model.

--- src/providers/dashscope/index.ts
+++ src/providers/dashscope/index.ts
@@ -8,8 +8,45 @@ import { ProviderConfigs } from '../types';
 import { dashscopeAPIConfig } from './api';
 
 export const DashScopeConfig: ProviderConfigs = {
-  chatComplete: chatCompleteParams([], { model: 'qwen-turbo' }),
+  chatComplete: chatCompleteParams(
+    [],
+    { model: 'qwen-turbo' },
+    {
+      top_k: {
+        param: 'top_k',
+      },
+      repetition_penalty: {
+        param: 'repetition_penalty',
+      },
+      stop: {
+        param: 'stop',
+      },
+      enable_search: {
+        param: 'enable_search',
+      },
+      enable_thinking: {
+        param: 'enable_thinking',
+      },
+      thinking_budget: {
+        param: 'thinking_budget',
+      },
+    }
+  ),
   embed: embedParams([], { model: 'text-embedding-v1' }),
+  rerank: {
+    model: {
+      param: 'model',
+    },
+    query: {
+      param: 'input.query',
+    },
+    documents: {
+      param: 'input.documents',
+    },
+    parameters: {
+      param: 'parameters',
+    },
+  },
   api: dashscopeAPIConfig,
   responseTransforms: responseTransformers(DASHSCOPE, {
     chatComplete: true,
