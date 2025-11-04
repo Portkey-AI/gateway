6bf69a298d6b3313caeb6ab3ed90e9c644a8e70f
siddharthsambharia-portkey <siddharth.s@portkey.ai>
2025-11-04 18:08:48 +0530

    fix

--- src/providers/anthropic/api.ts
+++ src/providers/anthropic/api.ts
@@ -2,16 +2,23 @@ import { ProviderAPIConfig } from '../types';
 
 const AnthropicAPIConfig: ProviderAPIConfig = {
   getBaseURL: () => 'https://api.anthropic.com/v1',
-  headers: ({ providerOptions, fn, headers: requestHeaders }) => {
+
+  headers: ({ providerOptions, fn, headers: requestHeaders, gatewayRequestBody }) => {
     const apiKey =
       providerOptions.apiKey || requestHeaders?.['x-api-key'] || '';
     const headers: Record<string, string> = {
       'X-API-Key': apiKey,
     };
-
+  
+    // Accept anthropic_beta and anthropic_version in body to support enviroments which cannot send it in headers.
     const betaHeader =
-      providerOptions?.['anthropicBeta'] ?? 'messages-2023-12-15';
-    const version = providerOptions?.['anthropicVersion'] ?? '2023-06-01';
+      providerOptions?.['anthropicBeta'] ??
+      gatewayRequestBody?.['anthropic_beta'] ??
+      'messages-2023-12-15';
+    const version =
+      providerOptions?.['anthropicVersion'] ??
+      gatewayRequestBody?.['anthropic_version'] ??
+      '2023-06-01';
 
     if (fn === 'chatComplete') {
       headers['anthropic-beta'] = betaHeader;
