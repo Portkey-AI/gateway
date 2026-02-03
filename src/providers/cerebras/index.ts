ef5fc0681d47ae435478526f26f077c447018d2c
Tiberiu Sabău <T.Sabau@student.tudelft.nl>
2026-02-03 13:51:36 +0100

    Add mapping for max_tokens for groq and cerebras

--- src/providers/cerebras/index.ts
+++ src/providers/cerebras/index.ts
@@ -4,14 +4,24 @@ import { ProviderConfigs } from '../types';
 import { cerebrasAPIConfig } from './api';
 
 export const cerebrasProviderAPIConfig: ProviderConfigs = {
-  chatComplete: chatCompleteParams([
-    'frequency_penalty',
-    'logit_bias',
-    'logprobs',
-    'presence_penalty',
-    'parallel_tool_calls',
-    'service_tier',
-  ]),
+  chatComplete: chatCompleteParams(
+    [
+      'frequency_penalty',
+      'logit_bias',
+      'logprobs',
+      'presence_penalty',
+      'parallel_tool_calls',
+      'service_tier',
+    ],
+    undefined,
+    {
+      max_completion_tokens: {
+        param: 'max_tokens',
+        default: 100,
+        min: 0,
+      },
+    }
+  ),
   api: cerebrasAPIConfig,
   responseTransforms: responseTransformers(CEREBRAS, {
     chatComplete: true,
