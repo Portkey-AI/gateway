b341133ff5e8272186e3cb491d54540202b88c97
Kenan Yildirim <kenan.yildirim@crowdstrike.com>
2026-05-11 16:06:29 -0400

    feat: forward request metadata to CrowdStrike AIDR
    Forward additional context to CrowdStrike AIDR's guard_chat_completions
    endpoint:
    
     - `user_id` from `metadata._user` or `metadata.user_id`
     - `llm_provider` from the request provider
     - `model` from the request body
     - `extra_info.user_name` from `metadata._user_name` or `metadata.user_name`

--- plugins/index.ts
+++ plugins/index.ts
@@ -66,6 +66,7 @@ import { handler as javelinguardrails } from './javelin/guardrails';
 import { handler as f5GuardrailsScan } from './f5-guardrails/scan';
 import { handler as azureShieldPrompt } from './azure/shieldPrompt';
 import { handler as azureProtectedMaterial } from './azure/protectedMaterial';
+import { handler as crowdstrikeAidrGuardChatCompletions } from './crowdstrike-aidr/guardChatCompletion';
 
 export const plugins = {
   default: {
@@ -175,5 +176,8 @@ export const plugins = {
   },
   'f5-guardrails': {
     scan: f5GuardrailsScan,
+  },
+  'crowdstrike-aidr': {
+    guardChatCompletions: crowdstrikeAidrGuardChatCompletions,
   },
 };
